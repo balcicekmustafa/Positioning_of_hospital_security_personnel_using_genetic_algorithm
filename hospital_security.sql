5d25ec218fb488f1f232146546b3af0cd5c1bd46
balcicekmustafa <balcicekmustafa46@gmail.com>
2024-09-17 12:45:48 +0300

    Database code updated

--- hospital_security.sql
+++ hospital_security.sql
@@ -39,6 +39,6 @@ CREATE TABLE PersonelAtama (
 CREATE TABLE Admin (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
-    password_hash VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL
 );
 
